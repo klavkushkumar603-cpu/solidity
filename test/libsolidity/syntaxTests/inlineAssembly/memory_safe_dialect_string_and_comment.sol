1b757db82ec8c4ba8e39939f7896b3b120b852a2
Matheus Aguiar <matheus.pit@gmail.com>
2025-08-18 20:42:58 -0300

    Update natspec warning tests

--- test/libsolidity/syntaxTests/inlineAssembly/memory_safe_dialect_string_and_comment.sol
+++ test/libsolidity/syntaxTests/inlineAssembly/memory_safe_dialect_string_and_comment.sol
@@ -5,3 +5,4 @@ function f() pure {
 }
 // ----
 // Warning 8544: (63-104): Inline assembly marked as memory safe using both a NatSpec tag and an assembly flag. If you are not concerned with backwards compatibility, only use the assembly flag, otherwise only use the NatSpec tag.
+// Warning 2424: (63-104): Natspec memory safe annotation for inline assembly will be deprecated in the next breaking version.
