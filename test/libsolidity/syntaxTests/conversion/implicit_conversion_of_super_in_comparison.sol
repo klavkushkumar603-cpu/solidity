2ece78b3b16f56dd3975285567df78120a77ce9f
Matheus Aguiar <matheus.pit@gmail.com>
2025-08-18 21:18:19 -0300

    Update contract type deprecation warning tests

--- test/libsolidity/syntaxTests/conversion/implicit_conversion_of_super_in_comparison.sol
+++ test/libsolidity/syntaxTests/conversion/implicit_conversion_of_super_in_comparison.sol
@@ -21,7 +21,10 @@ contract C {
 // ----
 // TypeError 2271: (144-157): Built-in binary operator != cannot be applied to types type(contract super C) and contract C.
 // TypeError 2271: (167-180): Built-in binary operator != cannot be applied to types contract C and type(contract super C).
+// Warning 9170: (167-180): Comparison of variables of contract type will be deprecated in the next breaking version.Consider using an explicit cast to address type.
 // TypeError 2271: (254-264): Built-in binary operator != cannot be applied to types type(contract super C) and contract C.
 // TypeError 2271: (274-284): Built-in binary operator != cannot be applied to types contract C and type(contract super C).
+// Warning 9170: (274-284): Comparison of variables of contract type will be deprecated in the next breaking version.Consider using an explicit cast to address type.
 // TypeError 2271: (349-359): Built-in binary operator != cannot be applied to types type(contract super C) and contract D.
 // TypeError 2271: (369-379): Built-in binary operator != cannot be applied to types contract D and type(contract super C).
+// Warning 9170: (369-379): Comparison of variables of contract type will be deprecated in the next breaking version.Consider using an explicit cast to address type.
